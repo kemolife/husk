80d32adc4d18fb33bb93c2628893bb96e26c1a5a
vitaliiantoniuk <vitalii@2solar.nl>
2026-05-29 08:52:53 +0200

    feat: add DoctrinePipelineRunRepository

---
+++ src/Infrastructure/Persistence/Doctrine/DoctrinePipelineRunRepository.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Infrastructure\Persistence\Doctrine;
+
+use App\Application\Port\PipelineRunRepositoryPort;
+use App\Domain\PipelineRun\PipelineRun;
+use App\Domain\PipelineRun\PipelineRunId;
+use App\Domain\PipelineRun\PipelineRunNotFoundException;
+use Doctrine\ORM\EntityManagerInterface;
+
+class DoctrinePipelineRunRepository implements PipelineRunRepositoryPort
+{
+    public function __construct(private readonly EntityManagerInterface $em) {}
+
+    public function save(PipelineRun $run): void
+    {
+        $this->em->persist($run);
+        $this->em->flush();
+    }
+
+    public function findById(PipelineRunId $id): PipelineRun
+    {
+        $run = $this->em->find(PipelineRun::class, $id->value);
+
+        if ($run === null) {
+            throw PipelineRunNotFoundException::forId($id->value);
+        }
+
+        return $run;
+    }
+}
